5c4ef4636a1407118c1c58429d7c3393d7fb6134
chetnavarma-DE <chetna.varma97@gmail.com>
2025-08-22 17:05:25 +0530

    Create flow_etl.py

---
+++ dags/flow_etl.py
@@ -0,0 +1,19 @@
+@dag(dag_id="etl_flow_demo1", start_date=datetime(2024,1,1), schedule="@daily", catchup=False)
+def etl():
+    @task
+    def extract():
+        return {"value": 100}
+
+    @task
+    def transform(record: dict):
+        record["value"] *= 2
+        return record
+
+    @task
+    def load(record: dict):
+        print(f"Loaded record: {record}")
+
+    load(transform(extract()))
+
+dag = etl()
+#update dag
